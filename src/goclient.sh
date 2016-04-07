1c03248d5a0d3b84907cf2421b262e60003af90a
Aaron Raddon <aaron@lytics.io>
2016-04-07 16:41:29 -0700

    First attempt at go proto, too much protobuf changes

--- src/goclient.sh
+++ src/goclient.sh
@@ -1,5 +1,8 @@
 
 
-protoc -I ./ ./kudu/tserver/tserver.proto --go_out=plugins=grpc:kudugo
+protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf:. --go_out=plugins=grpc:kudugo --gofast_out=. ./kudu/tserver/tserver_service.proto
+# protoc -I ./ ./kudu/tserver/tserver_service.proto --go_out=plugins=grpc:kudugo
 
+
+#protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf:. --gofast_out=. sql.proto
 
